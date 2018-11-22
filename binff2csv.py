6d5b72229758a58c330c234c482b7205475974fc
Evgeny Gamza <evgeny.gamza@gmail.com>
2018-11-22 08:53:33 +0300

    Scv writing in binff2scv was modified with pandas. Testing magnetogram file was created.

--- binff2csv.py
+++ binff2csv.py
@@ -7,17 +7,19 @@
 import numpy
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
-
+import pandas as pd
 
 Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
 
-
-# Let's take full-path names of files without extension to openlist
+# next block takes full-path names of files without extension to openlist
 openlist = [each[:-3] for each in askopenfilenames(
     title="Выберите *.ffd или *.ffh файл",
     filetypes=(("ffd files", "*.ffd"), ("ffh files", "*.ffh"), ("X-files", "X.*")))]
 
+# openlist = ['C:/Users/Evgeny/PycharmProjects/ffprocessing/test.']  # testing version of openlist
 
+
+# here's the main part of file:
 def binff2csv(filename):  # converting function
     # try to open files
     try:
@@ -29,7 +31,7 @@ def binff2csv(filename):  # converting function
     # reading first part of header file
     recsize = 0  # number of bytes in a single measure count
     Nrows = 0  # number of measurements
-    Ncols = 0  # number of columns
+    Ncols = 0  # number of colheaders
     Missvs = 0  # value flagging missing data
     for strin in ffh:  # reading
         value = strin.split('=')[-1]  # variable takes parameters while passing cycle
@@ -43,37 +45,41 @@ def binff2csv(filename):  # converting function
             Nrows = int(value)
         elif strin.find('MISSING') >= 0:
             Missvs = float(value)  # todo check out header file for other parameters
+    ffh.readline()
+    colheaders = []  # headers of our csv-table
+    for strin in ffh:
+        if strin.find('<') >= 0:
+            colheaders.append(strin.split('<')[1].split('>')[0])
+        else:
+            break
     ffh.close()
+
     print('file length is: %d counts' % Nrows)
-    print('number of columns: %d' % (Ncols + 1))
+    print('number of colheaders: %d' % (Ncols + 1))
     print('estimated file size: %d' % (recsize * Nrows))
 
     # open binary file
     ffd = open(filename + 'ffd', 'rb')
     dt = numpy.dtype([('time', 'f8'), ('data', repr(Ncols) + 'f4')])  # format of first array
-    n1 = 0
-    n2 = Nrows
-    a = numpy.fromfile(ffd, dtype=dt, count=int(n2 - n1))  # take array
-    b = []
+    n1 = 0  # first count
+    n2 = Nrows  # last count
+    a = numpy.fromfile(ffd, dtype=dt, count=int(n2 - n1))  # primary array
+    b = []  # flat array
     for row in a:  # convert array format to simple table
         rowlist = [row[0]]
         for value in row[1]:
             rowlist.append(value)
         b.append(rowlist)
 
-    # making .csv  todo check out header file for useful info and take it to new csv
+    df = pd.DataFrame(b, columns=colheaders)
+    print(df)
+
+    # making .csv
     outfile = open(filename + 'csv', 'w')
-    for row in b:
-        outfile.write('%14.8f;' % row[0])
-        for n in range(1, Ncols + 1):
-            if str(row[n]) == str(Missvs):  # fixme "elegant" decision, doesn't work without converting to str
-                outfile.write(';')
-            else:
-                outfile.write('%f;' % row[n])
-        outfile.write('\n')
+    df.to_csv(outfile, sep=';')
     outfile.close()
 
-
+# executing part of file
 for file in openlist:
     binff2csv(file)
 
