6744cf88fb617de853edeea342bd2273fd5dbff9
Evgeny Gamza <evgeny.gamza@gmail.com>
2018-11-30 14:31:10 +0300

    magnetogramms were brought to zero in 2 ways: by subtracting the mean from each value or by subtracting first valid value

--- csv_plotter.py
+++ csv_plotter.py
@@ -14,6 +14,8 @@ openlist = askopenfilenames(
     filetypes=(("csv files", "*.csv"),
                ("all files", "*.*")))
 
+root.destroy()  # closing the invisible window
+
 # # some openlists for testing:
 # # short single file:
 # openlist = ['C:/Users/Evgeny/PycharmProjects/ffprocessing/test.csv']
@@ -26,8 +28,6 @@ openlist = askopenfilenames(
 #             'C:/Users/Evgeny/YandexDisk/!ИЗМИРАН/Сентябрьские магнитограммы/ALK_20180900_60pp.csv',
 #             'C:/Users/Evgeny/PycharmProjects/ffprocessing/test.csv']
 
-root.destroy()  # closing the invisible window
-
 
 # main part of file
 def csv_plot(filename):  # reading and plotting function
@@ -35,18 +35,19 @@ def csv_plot(filename):  # reading and plotting function
     df = pd.read_csv(filename, sep=';', engine='python', encoding='utf8',
                      index_col='date and time', parse_dates=True)
     # "engine" and "encoding" parameters was added to solve the problem with cyrillic path to file
-    # todo make a filter for invalid values (10 sigma for example)
-    print(df)
+    # todo make a window filter for invalid values (00100)
     print(df.info())
-    # df.plot(subplots=True, figsize=(10, 5))
-    # # todo попробовать этим
-    df.filter(regex=r'ALK_H').plot(ax=axes[0])
-    df.filter(regex=r'ALK_E').plot(ax=axes[1], sharex=True)
-    df.filter(regex=r'ALK_Z').plot(ax=axes[2], sharex=True)
+    for col in df.columns:
+        df[col] -= df[col].mean()  # subtract average value from column
+        # df[col] -= df[col].loc[df[col].first_valid_index()]  # subtract first valid value from column
+
+    df.filter(regex=r'..._[HX]').plot(ax=axes[0])  # add components to relevant subplots
+    df.filter(regex=r'..._[EY]').plot(ax=axes[1])  # using regular expressions as a filter parameter
+    df.filter(regex=r'..._Z').plot(ax=axes[2])
 
 
 # executive part
-fig, axes = plt.subplots(3, 1, figsize=(10, 5))
+fig, axes = plt.subplots(3, 1, figsize=(10, 5), sharex=True)
 for file in openlist:
     csv_plot(file)
 plt.show()
