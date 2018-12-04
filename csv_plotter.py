912c24c8034d3109651e85b9a1f22de14f31719b
Evgeny Gamza <evgeny.gamza@gmail.com>
2018-12-04 16:57:53 +0300

    Пока что полная срань. Кнопочки отключают целые сабплоты вместо отдельных графиков

--- csv_plotter.py
+++ csv_plotter.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tkinter import Tk
 from tkinter.filedialog import askopenfilenames
 import matplotlib.pyplot as plt
+from matplotlib.widgets import CheckButtons
 
 root = Tk()  # Tkinter window
 root.withdraw()  # We make it invisible
@@ -28,6 +29,9 @@ root.destroy()  # closing the invisible window
 #             'C:/Users/Evgeny/YandexDisk/!ИЗМИРАН/Сентябрьские магнитограммы/ALK_20180900_60pp.csv',
 #             'C:/Users/Evgeny/PycharmProjects/ffprocessing/test.csv']
 
+llll = []
+nnnn = ['x', 'y', 'z']
+
 
 # main part of file
 def csv_plot(filename):  # reading and plotting function
@@ -45,13 +49,26 @@ def csv_plot(filename):  # reading and plotting function
         df.filter(regex=r'..._[EY]').plot(ax=axes[1])  # using regular expressions as a filter parameter
         df.filter(regex=r'..._Z').plot(ax=axes[2])
     except:
-        df.filter(regex=r'A[UEL]').plot(ax=axes[3])
+        llll.append(df.filter(regex=r'A[UEL]').plot(ax=axes[0]))  # fixme
+        # df.filter(regex=r'A[UEL]').plot(ax=axes[0])
 
 
 # executive part todo make a 3 or 4 subplots window
-fig, axes = plt.subplots(4, 1, figsize=(10, 5), sharex=True)
+def func(label):
+    for i in range(len(llll)):
+        if label == nnnn[i]:
+            llll[i].set_visible(not llll[i].get_visible())
+    plt.draw()
+
+
+fig, axes = plt.subplots(3, 1, figsize=(10, 5), sharex=True)
+plt.subplots_adjust(left=0.2)
 for file in openlist:
     csv_plot(file)
+
+rax = plt.axes([0.05, 0.4, 0.1, 0.15])
+check = CheckButtons(rax, nnnn, (True for i in llll))
+check.on_clicked(func)
 plt.style.use('ggplot')
 plt.show()
 
