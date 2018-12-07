b60466b0a8924ef496d98b5b17191e36062c8dcb
Evgeny Gamza <evgeny.gamza@gmail.com>
2018-12-07 17:55:07 +0300

    Добавлен слайсер, добавлен читальщик МАГА, на графиках AL Немного причесан код в csv_plotter

---
+++ IAGA2csv.py
@@ -0,0 +1,48 @@
+# -*- coding: cp1251 -*-
+
+import pandas as pd
+from tkinter import Tk
+from tkinter.filedialog import askopenfilenames
+import matplotlib.pyplot as plt
+
+root = Tk()  # Tkinter window
+root.withdraw()  # We make it invisible
+
+# next block takes full-path names of files without extension to openlist
+openlist = askopenfilenames(title="Âûבונטעו *.csv פאיכ",
+                            filetypes=(("txt files", "*.txt"), ("all files", "*.*")))
+
+root.destroy()  # closing the invisible window
+
+
+# main part of file
+def IAGA2csv(filename):  # reading and plotting function
+    print('Now processing:  %s' % filename.split('/')[-1])
+    file = open(filename, 'r')
+    for i in range(14):
+        file.readline()
+    arr = []
+    for string in file:
+        arr.append(string.split())
+    df = pd.DataFrame(arr)
+
+
+    # df = pd.read_csv(filename, sep=' ', engine='python', encoding='utf8',
+    #                  parse_dates=True,  # index_col='date and time'
+    #                  skiprows=19)
+    # # "engine" and "encoding" parameters were added to solve the problem with cyrillic path to file
+    # # todo make a "window" filter for invalid values (00100) (someday)
+    print(df.info())
+    # df.index.name = 'date and time'
+
+    # 4. finally we make *.csv
+    outfile = open(filename[:-3] + 'csv', 'w')
+    df.to_csv(outfile, sep=';')
+    outfile.close()
+
+
+# executive part
+for file in openlist:
+    IAGA2csv(file)
+
+print('Done')
--- csv_plotter.py
+++ csv_plotter.py
@@ -16,18 +16,6 @@ openlist = askopenfilenames(
 
 root.destroy()  # closing the invisible window
 
-# # some openlists for testing:
-# # short single file:
-# openlist = ['C:/Users/Evgeny/PycharmProjects/ffprocessing/test.csv']
-
-# # file with cyrillic path:
-# openlist = ['C:/Users/Evgeny/YandexDisk/!ИЗМИРАН/Сентябрьские магнитограммы/SPB_20180900_60pp.csv']
-
-# # list of three files:
-# openlist = ['C:/Users/Evgeny/YandexDisk/!ИЗМИРАН/Сентябрьские магнитограммы/SPB_20180900_60pp.csv',
-#             'C:/Users/Evgeny/YandexDisk/!ИЗМИРАН/Сентябрьские магнитограммы/ALK_20180900_60pp.csv',
-#             'C:/Users/Evgeny/PycharmProjects/ffprocessing/test.csv']
-
 
 # main part of file
 def csv_plot(filename):  # reading and plotting function
@@ -37,19 +25,22 @@ def csv_plot(filename):  # reading and plotting function
     # "engine" and "encoding" parameters were added to solve the problem with cyrillic path to file
     # todo make a "window" filter for invalid values (00100) (someday)
     print(df.info())
-    for col in df.columns:
-        df[col] -= df[col].mean()  # subtract average value from column
-        # df[col] -= df[col].loc[df[col].first_valid_index()]  # subtract first valid value from column
-    try:
+    if 'AU' in df:
+        df.filter(regex=r'AL').plot(ax=axes[0])  # df.filter(regex=r'AU').plot(ax=axes[0]), df.filter(regex=r'AE').plot(ax=axes[0])
+        df.filter(regex=r'AL').plot(ax=axes[1])  # df.filter(regex=r'AU').plot(ax=axes[1]), df.filter(regex=r'AE').plot(ax=axes[1])
+        df.filter(regex=r'AL').plot(ax=axes[2])  # df.filter(regex=r'AU').plot(ax=axes[2]), df.filter(regex=r'AE').plot(ax=axes[2])
+    else:
+        for col in df.columns:
+            df[col] -= df[col].mean()  # subtract average value from column
+            df[col] *= 10
+            # df[col] -= df[col].loc[df[col].first_valid_index()]  # subtract first valid value from column
         df.filter(regex=r'..._[HX]').plot(ax=axes[0])  # add components to relevant subplots
         df.filter(regex=r'..._[EY]').plot(ax=axes[1])  # using regular expressions as a filter parameter
         df.filter(regex=r'..._Z').plot(ax=axes[2])
-    except:
-        df.filter(regex=r'A[UEL]').plot(ax=axes[3])
 
 
 # executive part todo make a 3 or 4 subplots window
-fig, axes = plt.subplots(4, 1, figsize=(10, 5), sharex=True)
+fig, axes = plt.subplots(3, 1, figsize=(10, 5), sharex=True)
 for file in openlist:
     csv_plot(file)
 plt.style.use('ggplot')
